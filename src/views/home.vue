fb03e98716dc5c57f608f87b7c4bfd66fda24a9f
Holeski <huleski@gmail.com>
2019-09-30 10:49:57 +0800

    update

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,14 +2,15 @@
   <div style="margin-top: 20px;">
     <div class="block">
       <el-date-picker
-        v-model="value1"
+        v-model="currentDate"
         type="date"
         placeholder="选择日期"
         value-format="yyyy-MM-dd"
         @change="init"
+        :picker-options="pickerOptions"
       ></el-date-picker>
     </div>
-    <button type="button" class="el-carousel__arrow el-carousel__arrow--left">
+    <button type="button" class="el-carousel__arrow el-carousel__arrow--left" @click="changeDate(-1)">
       <i class="el-icon-arrow-left"></i>
     </button>
     <button type="button" class="el-carousel__arrow el-carousel__arrow--right">
@@ -41,22 +42,38 @@ export default {
   components: {},
   data() {
     return {
+      pickerOptions: {
+        disabledDate(time) {
+          return time.getTime() > Date.now();
+        }
+      },
       list: [],
       over: false,
-      value1: ""
+      currentDate: ""
     };
   },
   created() {},
   mounted() {
     this.init();
+    this.currentDate = new Date();
+    this.currentDate.setDate(new Date().getDate() - 2);
     let _this = this;
 
     // 添加点击事件
     document.getElementById("waterfall").addEventListener("click", function(e) {
-      if (e.target.attributes["son-idx"] && !(e.target.attributes["son-idx"].nodeValue === null || e.target.attributes["son-idx"].nodeValue === "") ) {
+      if (
+        e.target.attributes["son-idx"] &&
+        !(
+          e.target.attributes["son-idx"].nodeValue === null ||
+          e.target.attributes["son-idx"].nodeValue === ""
+        )
+      ) {
         let idx = parseInt(e.target.attributes["son-idx"].nodeValue);
         _this.showBigAction(idx);
-      } else if (e.target.attributes["class"] && e.target.attributes["class"].value.indexOf("description") != -1 ) {
+      } else if (
+        e.target.attributes["class"] &&
+        e.target.attributes["class"].value.indexOf("description") != -1
+      ) {
         if (e.target.attributes["class"].value.indexOf("checked") == -1) {
           e.target.classList.add("checked");
         } else {
@@ -66,21 +83,27 @@ export default {
     });
 
     // 添加右键点击事件
-    document.getElementById("waterfall").addEventListener("contextmenu", function(e) {
-      event.preventDefault();
-      console.log(e.target.attributes["class"].value)
-      if (e.target.attributes["class"] && e.target.attributes["class"].value== "img") {
-        let parent = e.target.parentNode;
-        if (parent.attributes["class"].value.indexOf("green-bord") == -1) {
-          parent.classList.add("green-bord");
-        } else {
-          parent.classList.remove("green-bord");
+    document
+      .getElementById("waterfall")
+      .addEventListener("contextmenu", function(e) {
+        event.preventDefault();
+        
+        if (
+          e.target.attributes["class"] &&
+          e.target.attributes["class"].value == "img"
+        ) {
+          let parent = e.target.parentNode;
+          if (parent.attributes["class"].value.indexOf("green-bord") == -1) {
+            parent.classList.add("green-bord");
+          } else {
+            parent.classList.remove("green-bord");
+          }
         }
-      }
-    });
+      });
   },
   methods: {
     init() {
+      // 初始化
       this.list = [];
       document.getElementById("waterfall").innerHTML = "";
 
@@ -100,11 +123,11 @@ export default {
         _this.loadMore();
       });
     },
-    loadMore() {
+    loadMore() { // 加载数据
       // 没有数据
       if (this.over) return;
       let _this = this;
-
+      
       getData().then(res => {
         let dataArr = res.data.data;
         if (!dataArr || dataArr.length < 1) {
@@ -145,7 +168,7 @@ export default {
         waterfall.append(arr.join(""), ".img");
       });
     },
-    showBigAction(idx) {
+    showBigAction(idx) { // 图片放大预览
       let tmp = [];
       this.list.forEach(ele => {
         tmp.push(ele.src.large);
@@ -155,24 +178,26 @@ export default {
         index: idx
       });
     },
-    rightClick(item) {
-      console.log(item);
-    },
-    toTop() {
-      var gotoTop= function(){
-          var currentPosition = document.documentElement.scrollTop || document.body.scrollTop;
-          currentPosition -= 100;
-          if (currentPosition > 0) {
-            window.scrollTo(0, currentPosition);
-          }
-          else {
-            window.scrollTo(0, 0);
-            clearInterval(timer);
-            timer = null;
-          }
+    toTop() { // 回到顶部
+      var gotoTop = function() {
+        var currentPosition =
+          document.documentElement.scrollTop || document.body.scrollTop;
+        currentPosition -= 300;
+        if (currentPosition > 0) {
+          window.scrollTo(0, currentPosition);
+        } else {
+          window.scrollTo(0, 0);
+          clearInterval(timer);
+          timer = null;
         }
-        var timer=setInterval(gotoTop,1);
+      };
+      var timer = setInterval(gotoTop, 1);
       // window.scrollTo(0, 0);
+    },
+    changeDate(i) {
+      let tmp = this.currentDate;
+      this.currentDate.setDate(tmp.getDate() + i);
+      console.log(this.currentDate)
     }
   }
 };
@@ -215,6 +240,6 @@ function clone(origin, target) {
   display: inline-block;
 }
 .green-bord {
-  border: 3px lime solid
+  border: 3px lime solid;
 }
 </style>
